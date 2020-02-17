31d03e6c1255935dea2175f8c6a0b622d28c3f9c
chbu <chbu@dtu.dk>
2020-02-17 16:28:04 +0100

    Initial commit

---
+++ src/main/java/FX2.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class FX2 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello FX13");
+        FlowPane flowPane = new FlowPane();
+        flowPane.getChildren().add(label);
+        Scene scene = new Scene(flowPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
