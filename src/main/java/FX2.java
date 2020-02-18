6f23373c644d8b4c2872800defbbc4e804e00026
chbu <chbu@dtu.dk>
2020-02-18 08:56:21 +0100

    Skiftet tekst

--- src/main/java/FX2.java
+++ src/main/java/FX2.java
@@ -3,16 +3,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-
 public class FX2 extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello FX13");
+        Label label = new Label("Hello FX13 - Commit Nr 2!");
         FlowPane flowPane = new FlowPane();
         flowPane.getChildren().add(label);
         Scene scene = new Scene(flowPane);
